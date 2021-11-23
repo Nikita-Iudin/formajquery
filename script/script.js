39067df535da9a6482c382f2d47cdf73699fbca7
Nikita-Iudin <75238354+Nikita-Iudin@users.noreply.github.com>
2021-11-23 10:36:06 +0400

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,27 +1,25 @@
-$(document).ready(function()
-{
-        $("#submit").click(function()
-        {
-                $("#erconts").fadeIn(500);
-                $.ajax(
-                {
+$(document).ready(function () {
+        $('#callbacks').on('submit', function (e) {
+                e.preventDefault();
+                $.ajax({
                         type: "POST",
-                        url: "send.php", // Адрес обработчика
+                        url: "send.php",
                         data: $("#callbacks").serialize(),
-                        error:function()
-                        {
-                                $("#erconts").html("Произошла ошибка!");
+                        error: function () {
+                                setTimeout(() => {
+                                        $("#erconts").html("Произошла ошибка!");
+                                }, 1000);
                         },
-                        beforeSend: function()
-                        {
+                        beforeSend: function () {
                                 $("#erconts").html("Отправляем данные...");
                         },
-                        success: function(result)
-                        {
-                                $("#erconts").html(result);
-                                checkThis();
+                        success: function (result) {
+                                document.querySelector('.submit-btn').classList.add("submitted")
+                                setTimeout(() => {
+                                        $("#erconts").html(result);
+                                }, 1000);
                         }
                 });
-        return false;
+                return false;
         });
 });
